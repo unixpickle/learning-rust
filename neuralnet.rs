b5378b3513639cc879a4894fd8df5bde0d4299e5
Alex Nichol <unixpickle@gmail.com>
2018-02-26 21:18:11 -0800

    macro for applying operator to Tensor
    This is so convenient, holy cow!

--- neuralnet.rs
+++ neuralnet.rs
@@ -1,51 +1,65 @@
 // Toy neural network training in Rust.
 
+use std::ops::Add;
+
 // An N-dimensional array of floating-point values.
+#[derive(Clone)]
 struct Tensor {
     data: Vec<f32>,
     shape: Vec<usize>
+}
+
+macro_rules! tensor_op {
+    ($t1:expr, $op:tt, $t2:expr) => {
+        {
+            let t1 = $t1;
+            let t2 = $t2;
+            let mut result = Tensor{data: Vec::<f32>::new(), shape: t1.shape.clone()};
+            if t1.shape != t2.shape || t1.data.len() != t2.data.len() {
+                panic!("shape mismatch");
+            }
+            for i in 0..t1.data.len() {
+                result.data.push(t1.data[i] $op t2.data[i]);
+            }
+            result
+        }
+    }
 }
 
 // A tensor that can be back-propagated through.
 trait Res {
     fn value(&self) -> &Tensor;
 
-    fn backward(self, out_grad: &Tensor);
+    fn backward(&mut self, out_grad: &Tensor);
 }
 
-struct Sum<A: Res, B: Res> {
-    a: A,
-    b: B,
+struct Sum {
+    a: Box<Res>,
+    b: Box<Res>,
     sum: Tensor
 }
 
-impl<A: Res, B: Res> Sum<A, B> {
-    fn new(a: A, b: B) -> Result<Sum<A, B>, String> {
-        if a.value().shape != b.value().shape {
-            return Err("input shapes do not match".into());
-        }
-        let mut sum_data = Vec::<f32>::new();
-        let size = a.value().data.len();
-        for i in 0..size {
-            sum_data.push(a.value().data[i] + b.value().data[i]);
-        }
-        let tensor = Tensor{data: sum_data, shape: a.value().shape.clone()};
-        Ok(Sum{
-            a: a,
-            b: b,
-            sum: tensor
-        })
-    }
-}
-
-impl<A: Res, B: Res> Res for Sum<A, B> {
+impl Res for Sum {
     fn value(&self) -> &Tensor {
         &self.sum
     }
 
-    fn backward(self, out_grad: &Tensor) {
+    fn backward(&mut self, out_grad: &Tensor) {
         self.a.backward(out_grad);
         self.b.backward(out_grad);
+    }
+}
+
+impl Add for Box<Res> {
+    type Output = Box<Res>;
+
+    fn add(self, b: Box<Res>) -> Box<Res> {
+        let tensor = tensor_op!(self.value(), +, b.value());
+        Box::new(Sum{
+            a: self,
+            b: b,
+            sum: tensor
+        })
     }
 }
 
