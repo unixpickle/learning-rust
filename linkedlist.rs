dfaaba17138f9f8269e74d82a50dbf6aa77782e5
Alex Nichol <unixpickle@gmail.com>
2022-08-21 13:27:28 -0700

    linkedlist shift/unshift works

--- linkedlist.rs
+++ linkedlist.rs
@@ -6,7 +6,12 @@ use std::mem::{take};
 use std::rc::{Weak, Rc};
 
 struct Node<T> {
-    data: T,
+    // I can't figure out a way to make this not an Option.
+    // When we pop an item, we would need to consume the data
+    // to return it, which would need to consume the Node<T>,
+    // which doesn't seem possible if the Node<T> is in an Rc.
+    data: Option<T>,
+
     prev: Option<Weak<RefCell<Node<T>>>>,
     next: Option<Rc<RefCell<Node<T>>>>,
 }
@@ -23,7 +28,7 @@ impl<T> LinkedList<T> {
 
     fn unshift(&mut self, item: T) {
         let new_head = Rc::new(RefCell::new(Node{
-            data: item,
+            data: Some(item),
             prev: None,
             next: None,
         }));
@@ -39,9 +44,42 @@ impl<T> LinkedList<T> {
             }
         }
     }
+
+    fn shift(&mut self) -> Option<T> {
+        match take(&mut self.head) {
+            Some(old_head) => {
+                let mut obj = old_head.borrow_mut();
+                match take(&mut obj.next) {
+                    None => {
+                        self.tail = None;
+                    },
+                    Some(x) => {
+                        x.borrow_mut().prev = None;
+                        self.head = Some(x);
+                    }
+                };
+                take(&mut obj.data)
+            },
+            None => {
+                None
+            }
+        }
+    }
 }
 
 fn main() {
     let mut ll = LinkedList::<i32>::new();
-    ll.unshift(32);
+    for i in 0..10 {
+        ll.unshift(i);
+    }
+    loop {
+        match ll.shift() {
+            Some(x) => {
+                println!("hello: {}", x);
+            },
+            None => {
+                break;
+            },
+        };
+    }
 }
