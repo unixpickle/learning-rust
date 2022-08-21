64dea805103f937b2fc170d076c0056f5b0bae69
Alex Nichol <unixpickle@gmail.com>
2022-08-21 13:15:09 -0700

    fix build errors

--- linkedlist.rs
+++ linkedlist.rs
@@ -1,18 +1,19 @@
 // A doubly linked-list implementation, to help me understand
 // weak references, refcells, etc.
 
-use std::rc::{Weak, Rc};
+use std::cell::{RefCell};
 use std::mem::{take};
+use std::rc::{Weak, Rc};
 
 struct Node<T> {
-    data: T
-    prev: Option<Weak<RefCell<Node<T>>>>
-    next: Option<Rc<RefCell<Node<T>>>>
+    data: T,
+    prev: Option<Weak<RefCell<Node<T>>>>,
+    next: Option<Rc<RefCell<Node<T>>>>,
 }
 
 struct LinkedList<T> {
-    head: Option<Rc<RefCell<Node<T>>>>
-    tail: Option<Weak<RefCell<Node<T>>>>>
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Weak<RefCell<Node<T>>>>,
 }
 
 impl<T> LinkedList<T> {
@@ -21,28 +22,26 @@ impl<T> LinkedList<T> {
     }
 
     fn unshift(&mut self, item: T) {
-        match take(self.head) {
+        let new_head = Rc::new(RefCell::new(Node{
+            data: item,
+            prev: None,
+            next: None,
+        }));
+        match take(&mut self.head) {
             Some(old_head) => {
-                self.head = Some(Rc::new(RefCell::new(Node{
-                    data: item,
-                    prev: None,
-                    next: Some(old_head),
-                })))
-                self.head.next.borrow_mut().prev = self.head.unwrap().downgrade();
+                old_head.borrow_mut().prev = Some(Rc::downgrade(&new_head));
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
             },
             None => {
-                self.head = Some(Rc::new(RefCell::new(Node{
-                    data: item,
-                    prev: None,
-                    next: None,
-                })));
-                self.tail = self.head.unwrap().downgrade();
+                self.tail = Some(Rc::downgrade(&new_head));
+                self.head = Some(new_head);
             }
         }
     }
 }
 
 fn main() {
-    let ll = LinkedList<i32>::new();
+    let mut ll = LinkedList::<i32>::new();
     ll.unshift(32);
 }
