0d0c28b24cd9e888fba6dee05d92179d33f72175
Alex Nichol <unixpickle@gmail.com>
2022-08-26 17:58:41 -0700

    better error handling; forward body & content-type

--- http_proxy/src/main.rs
+++ http_proxy/src/main.rs
@@ -1,9 +1,12 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server, Uri};
 use hyper::client::Client;
+use hyper::header::CONTENT_TYPE;
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, Uri};
+use std::convert::Infallible;
 use std::env::args;
+use std::error::Error as StdError;
+use std::fmt;
+use std::net::SocketAddr;
 use std::process::ExitCode;
 
 #[tokio::main]
@@ -32,19 +35,58 @@ async fn main() -> ExitCode {
     ExitCode::SUCCESS
 }
 
-async fn forward_request<'a>(req: Request<Body>, destination_url: String) -> Result<Response<Body>, Infallible> {
-    let destination_uri = destination_url.parse::<Uri>().unwrap();
+async fn forward_request(
+    req: Request<Body>,
+    destination_url: String,
+) -> Result<Response<Body>, Infallible> {
+    match forward_request_or_fail(req, destination_url).await {
+        Ok(res) => Ok(res),
+        Err(err) => Ok(Response::new(Body::from(format!("{}", err)))),
+    }
+}
+
+async fn forward_request_or_fail(
+    req: Request<Body>,
+    destination_url: String,
+) -> Result<Response<Body>, GenericError> {
+    let destination_uri = destination_url.parse::<Uri>()?;
     let source_uri = req.uri().clone();
     let forward_uri = Uri::builder()
         .scheme(destination_uri.scheme().unwrap().clone())
         .authority(destination_uri.authority().unwrap().clone())
         .path_and_query(source_uri.path_and_query().unwrap().clone())
-        .build()
-        .unwrap();
+        .build()?;
 
     let client = Client::new();
-    match client.get(forward_uri).await {
-        Ok(value) => Ok(value),
-        Err(err) => Ok(Response::new(Body::from(format!("{}", err)))),
+
+    let mut builder = Request::builder()
+        .method(req.method().clone())
+        .uri(forward_uri);
+
+    if let Some(content_type) = req.headers().get(CONTENT_TYPE) {
+        builder = builder.header(CONTENT_TYPE, content_type)
+    }
+
+    let new_req = builder.body(req.into_body())?;
+    let response = client.request(new_req).await?;
+    Ok(response)
+}
+
+#[derive(Debug, Clone)]
+struct GenericError {
+    msg: String,
+}
+
+impl fmt::Display for GenericError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+impl<E: StdError> From<E> for GenericError {
+    fn from(x: E) -> Self {
+        GenericError {
+            msg: format!("hyper error: {}", x),
+        }
     }
 }
