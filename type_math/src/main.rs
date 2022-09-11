5d5129f7f565beaeb965e2dff5eecbf11dfad1a5
Alex Nichol <unixpickle@gmail.com>
2022-09-11 12:40:45 -0700

    products work

--- type_math/src/main.rs
+++ type_math/src/main.rs
@@ -1,3 +1,5 @@
+#![recursion_limit = "10000"]
+
 use std::marker::PhantomData;
 
 type N1 = Inc<N0>;
@@ -10,11 +12,16 @@ type N7 = Inc<N6>;
 type N8 = Inc<N7>;
 type N9 = Inc<N8>;
 type N10 = Inc<N9>;
-// type N100 = <N10 as Mul<N10>>::Prod;
+type N100 = <N10 as Mul<N10>>::Prod;
 
 fn main() {
     println!("2+3 = {}", <N2 as Add<N3>>::Sum::VALUE);
     println!("2+3+4 = {}", <N2 as Add<<N3 as Add<N4>>::Sum>>::Sum::VALUE);
+    println!("37 = {}", <<N3 as Mul<N10>>::Prod as Add<N7>>::Sum::VALUE);
+    println!(
+        "237 = {}",
+        <<N2 as Mul<N100>>::Prod as Add<<<N3 as Mul<N10>>::Prod as Add<N7>>::Sum>>::Sum::VALUE
+    );
 }
 
 trait Nat {
@@ -48,14 +55,17 @@ impl<Inner: Nat + Add<T>, T: Nat> Add<T> for Inc<Inner> {
     type Sum = Inc<Inner::Sum>;
 }
 
-// trait Mul<T: Nat> {
-//     type Prod: Nat + Add<T>;
-// }
+trait Mul<T: Nat> {
+    type Prod: Nat;
+}
 
-// impl<T: Nat + Add<T>> Mul<T> for N0 {
-//     type Prod = N0;
-// }
+impl<T: Nat> Mul<T> for N0 {
+    type Prod = N0;
+}
 
-// impl<Inner: Nat + Mul<T>, T: Nat> Mul<T> for Inc<Inner> {
-//     type Prod = <<Inner as Mul<T>>::Prod as Add<T>>::Sum;
-// }
+impl<Inner: Nat + Mul<T>, T: Nat> Mul<T> for Inc<Inner>
+where
+    T: Add<<Inner as Mul<T>>::Prod>,
+{
+    type Prod = <T as Add<<Inner as Mul<T>>::Prod>>::Sum;
+}
