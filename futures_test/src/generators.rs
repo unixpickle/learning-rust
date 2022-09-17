b781d11f1081eb3519c7826462a92d62e66ba2ad
Alex Nichol <unixpickle@gmail.com>
2022-09-17 12:54:47 -0700

    generators at least compiles

--- futures_test/src/generators.rs
+++ futures_test/src/generators.rs
@@ -1,26 +1,25 @@
 use futures_core::Stream;
 use std::future::Future;
 use std::mem::take;
-use std::ops::DerefMut;
 use std::{
     pin::Pin,
     sync::{Arc, Mutex},
     task::{Context, Poll},
 };
 
-struct YielderState<T> {
+struct YielderState<T: Unpin> {
     value: Option<T>,
 }
 
-pub struct YielderFuture<T> {
+pub struct YielderFuture<T: Unpin> {
     state: Arc<Mutex<YielderState<T>>>,
 }
 
-impl<T> Future for YielderState<T> {
+impl<T: Unpin> Future for YielderFuture<T> {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        if self.deref_mut().state.lock().unwrap().is_some() {
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.state.lock().unwrap().value.is_some() {
             Poll::Pending
         } else {
             Poll::Ready(())
@@ -28,33 +27,37 @@ impl<T> Future for YielderState<T> {
     }
 }
 
-pub struct Yielder<T> {
+pub struct Yielder<T: Unpin> {
     state: Arc<Mutex<YielderState<T>>>,
 }
 
-impl<T> YielderState<T> {
+impl<T: Unpin> Yielder<T> {
     fn put(&mut self, x: T) -> YielderFuture<T> {
-        let locked = self.state.lock().unwrap();
+        let mut locked = self.state.lock().unwrap();
         if locked.value.is_some() {
             panic!("cannot yield more than once without awaiting the previous result");
+        }
+        locked.value = Some(x);
+        YielderFuture {
+            state: self.state.clone(),
         }
     }
 }
 
-pub struct YielderStream<T, Fut: Future> {
+pub struct YielderStream<T: Unpin, Fut: Future> {
     future: Pin<Box<Fut>>,
     future_done: bool,
     state: Arc<Mutex<YielderState<T>>>,
 }
 
-impl<T, Fut: Future> Stream for YielderStream<T, Fut> {
+impl<T: Unpin, Fut: Future> Stream for YielderStream<T, Fut> {
     type Item = T;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         {
             // It is possible the Yielder was moved outside of the
             // future and yield was called on it externally.
-            let locked = self.state.lock().unwrap();
+            let mut locked = self.state.lock().unwrap();
             if let Some(x) = take(&mut locked.value) {
                 return Poll::Ready(Some(x));
             }
@@ -66,17 +69,30 @@ impl<T, Fut: Future> Stream for YielderStream<T, Fut> {
             self.future_done = true;
         }
 
-        let locked = self.state.lock().unwrap();
+        let mut locked = self.state.lock().unwrap();
         if let Some(x) = take(&mut locked.value) {
             Poll::Ready(Some(x))
         } else {
-            Poll::Pending
+            if self.future_done {
+                Poll::Ready(None)
+            } else {
+                Poll::Pending
+            }
         }
     }
 }
 
-pub fn make_stream<T, F, Fut: Future<Output = ()>>(f: F) -> YielderStream<T, Fut>
+pub fn make_stream<T: Unpin, F, Fut: Future<Output = ()>>(f: F) -> YielderStream<T, Fut>
 where
-    F: FnMut(Yielder<T>) -> Fut,
+    F: FnOnce(Yielder<T>) -> Fut,
 {
+    let state = Arc::new(Mutex::new(YielderState { value: None }));
+    let yielder = Yielder {
+        state: state.clone(),
+    };
+    YielderStream {
+        future: Box::pin(f(yielder)),
+        future_done: false,
+        state: state,
+    }
 }
