779757ee8432fa69514d28b0796d77eac01c477e
Alex Nichol <unixpickle@gmail.com>
2022-09-25 11:17:59 -0700

    save multiple matrix types

--- map_dump/src/cooccurrence.rs
+++ map_dump/src/cooccurrence.rs
@@ -2,10 +2,10 @@ use crate::{
     bing_maps::MapItem,
     geo_coord::{GeoCoord, VecGeoCoord},
 };
-use ndarray::{Array1, Array2};
+use ndarray::{Array1, Array2, LinalgScalar};
 use rand::{rngs::StdRng, SeedableRng};
-use serde_json::Value;
-use std::{collections::HashMap, path::PathBuf};
+use serde_json::{Map, Value};
+use std::{collections::HashMap, ops::AddAssign, path::PathBuf};
 
 use clap::Parser;
 use tokio::{
@@ -32,9 +32,6 @@ pub struct CoocurrenceArgs {
     // Default radius is roughly 10km
     #[clap(short, long, value_parser, default_value_t = 0.001569612306)]
     radius: f64,
-
-    #[clap(long, value_parser)]
-    single_count: bool,
 
     #[clap(short, long, value_parser, default_value_t = 8)]
     workers: i32,
@@ -76,12 +73,11 @@ pub async fn cooccurrence(cli: CoocurrenceArgs) -> anyhow::Result<()> {
         let results_tx_clone = results_tx.clone();
         let locations_clone = sorted_locations.clone();
         let radius = cli.radius;
-        let single_count = cli.single_count;
         spawn_blocking(move || {
             let mut rng = StdRng::from_rng(rand::thread_rng()).unwrap();
             loop {
                 let query = GeoCoord::random(&mut rng);
-                let covec = cooccurrence_at(&locations_clone, query, radius, single_count);
+                let covec = matrices_in_region(&locations_clone, query, radius);
                 if !results_tx_clone.blocking_send(covec).is_ok() {
                     return;
                 }
@@ -93,11 +89,11 @@ pub async fn cooccurrence(cli: CoocurrenceArgs) -> anyhow::Result<()> {
 
     println!("computing cooccurrence matrix...");
     let n = sorted_locations.len();
-    let mut result = Array2::<f32>::zeros((n, n));
+    let mut matrices = Matrices::zeros(n);
     let mut completed: i64 = 0;
     while completed < cli.samples {
         let sub_result = results_rx.recv().await.unwrap();
-        result += &sub_result;
+        matrices.add(&sub_result);
         completed += 1;
         if completed % (cli.samples / 1000).max(1) == 0 {
             println!(
@@ -108,13 +104,12 @@ pub async fn cooccurrence(cli: CoocurrenceArgs) -> anyhow::Result<()> {
             );
         }
     }
-    result /= completed as f32;
 
     // Tell the workers to stop sending results.
     drop(results_rx);
 
     println!("serializing resulting matrix to {}...", cli.output_path);
-    let serialized = serialize_matrix(sorted_names, result)?;
+    let serialized = serde_json::to_string(&matrices.into_json(sorted_names))?;
     let mut writer = File::create(cli.output_path).await?;
     writer.write_all(serialized.as_bytes()).await?;
     writer.flush().await?;
@@ -122,7 +117,7 @@ pub async fn cooccurrence(cli: CoocurrenceArgs) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub async fn read_locations(src: PathBuf) -> anyhow::Result<Vec<VecGeoCoord>> {
+async fn read_locations(src: PathBuf) -> anyhow::Result<Vec<VecGeoCoord>> {
     let mut reader = File::open(src).await?;
     let mut data = String::new();
     reader.read_to_string(&mut data).await?;
@@ -135,43 +130,96 @@ pub async fn read_locations(src: PathBuf) -> anyhow::Result<Vec<VecGeoCoord>> {
     Ok(result?)
 }
 
-pub fn cooccurrence_at(
+fn matrices_in_region(
     locations: &Vec<Vec<VecGeoCoord>>,
     query_point: GeoCoord,
     radius: f64,
-    single_count: bool,
-) -> Array2<f32> {
-    let vec_query = query_point.into();
-    let cos_radius = radius.cos();
+) -> Matrices {
     let n = locations.len();
-    let mut counts = Array1::<f32>::zeros((n,));
+
+    // Cache some computation for a speedup.
+    let cos_radius = radius.cos();
+    let vec_query = query_point.into();
+
+    let mut counts = Array1::<f64>::zeros((n,));
+    let mut one_hot = Array1::<f64>::zeros((n,));
     {
-        let slice = counts.as_slice_mut().unwrap();
         for (i, store_locations) in locations.iter().enumerate() {
             for store_location in store_locations {
                 if store_location.cos_geo_dist(&vec_query) > cos_radius {
-                    if single_count {
-                        slice[i] = 1.0;
-                    } else {
-                        slice[i] = slice[i] + 1.0;
-                    }
+                    one_hot[i] = 1.0;
+                    counts[i] = counts[i] + 1.0;
                 }
             }
         }
     }
-    let column = counts.clone().into_shape((n, 1)).unwrap();
-    let row = counts.into_shape((1, n)).unwrap();
+    let counts_outer = outer_product(counts);
+    Matrices {
+        cooccurrence: counts_outer.clone(),
+        correlation: outer_product(one_hot),
+        second_moment: counts_outer,
+        count: 1,
+    }
+}
+
+fn outer_product<T: LinalgScalar>(vec: Array1<T>) -> Array2<T> {
+    let n = vec.shape()[0];
+    let column = vec.clone().into_shape((n, 1)).unwrap();
+    let row = vec.into_shape((1, n)).unwrap();
     return column.dot(&row);
 }
 
-pub fn serialize_matrix(names: Vec<String>, result: Array2<f32>) -> anyhow::Result<String> {
-    let matrix_vec: Vec<Vec<f32>> = result
+struct Matrices {
+    cooccurrence: Array2<f64>,
+    correlation: Array2<f64>,
+    second_moment: Array2<f64>,
+    count: usize,
+}
+
+impl Matrices {
+    fn zeros(n: usize) -> Matrices {
+        Matrices {
+            cooccurrence: Array2::zeros((n, n)),
+            correlation: Array2::zeros((n, n)),
+            second_moment: Array2::zeros((n, n)),
+            count: 0,
+        }
+    }
+
+    fn add(&mut self, other: &Matrices) {
+        self.cooccurrence.add_assign(&other.cooccurrence);
+        self.correlation.add_assign(&other.correlation);
+        self.second_moment.add_assign(&other.second_moment);
+        self.count += other.count;
+    }
+
+    fn into_json(mut self, names: Vec<String>) -> Value {
+        let correlation_denom = outer_product(self.correlation.clone().into_diag().to_owned())
+            .map(|x| x.sqrt().max(1e-8));
+        self.correlation = self.correlation / correlation_denom;
+        self.second_moment /= self.count as f64;
+        Map::from_iter([
+            (
+                "cooccurrence".to_owned(),
+                Value::from(matrix_vec(self.cooccurrence)),
+            ),
+            (
+                "correlation".to_owned(),
+                Value::from(matrix_vec(self.correlation)),
+            ),
+            (
+                "second_moment".to_owned(),
+                Value::from(matrix_vec(self.second_moment)),
+            ),
+            ("names".to_owned(), names.into()),
+        ])
+        .into()
+    }
+}
+
+fn matrix_vec<T: LinalgScalar>(matrix: Array2<T>) -> Vec<Vec<T>> {
+    return matrix
         .outer_iter()
         .map(|x| x.iter().map(|x| *x).collect())
         .collect();
-    let obj = HashMap::from([
-        ("matrix", Value::from(matrix_vec)),
-        ("names", Value::from(names)),
-    ]);
-    Ok(serde_json::to_string(&obj)?)
 }
